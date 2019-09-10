c97a30b75f0f6b06e74e5b338fa5c9ec115a3f66
Leonid Shamis <leonid.shamis@gmail.com>
2019-09-10 14:32:15 -0700

    pep8

--- a0.py
+++ a0.py
@@ -11,10 +11,13 @@ class aio_sub:
         ns.q = asyncio.Queue(1)
         ns.cv = threading.Condition()
         ns.closing = False
-        
-        # Note: To prevent cyclic dependencies, `callback` is NOT owned by self.
+
+        # Note: To prevent cyclic dependencies, `callback` is NOT owned by
+        # self.
         def callback(pkt):
-            ns.loop.call_soon_threadsafe(lambda: asyncio.ensure_future(ns.q.put(pkt), loop=ns.loop))
+            ns.loop.call_soon_threadsafe(
+                lambda: asyncio.ensure_future(
+                    ns.q.put(pkt), loop=ns.loop))
             ns.cv.acquire()
             ns.cv.wait()
             ns.cv.release()
@@ -24,11 +27,10 @@ class aio_sub:
 
     def __aiter__(self):
         return self
-    
+
     async def __anext__(self):
         pkt = await self._ns.q.get()
         self._ns.cv.acquire()
         self._ns.cv.notify()
         self._ns.cv.release()
         return pkt
-
