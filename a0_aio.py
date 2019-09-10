77885b167564cc8702b57d975ba17c56049faf03
Leonid Shamis <leonid.shamis@gmail.com>
2019-09-10 13:52:29 -0700

    Start adding asyncio wrapper

---
+++ a0_aio.py
@@ -0,0 +1,33 @@
+import a0
+import asyncio
+import threading
+import types
+
+
+class Subscribe:
+    def __init__(self, shm, init_, iter_, loop=None):
+        ns = types.SimpleNamespace()
+        ns.loop = loop or asyncio.get_event_loop()
+        ns.q = asyncio.Queue(1)
+        ns.cv = threading.Condition()
+        ns.closing = False
+        
+        # Note: To prevent cyclic dependencies, `callback` is NOT owned by self.
+        def callback(pkt):
+            ns.loop.call_soon_threadsafe(lambda: asyncio.ensure_future(ns.q.put(pkt), loop=ns.loop))
+            ns.cv.acquire()
+            ns.cv.wait()
+            ns.cv.release()
+
+        self._ns = ns
+        self._sub = a0.Subscriber(shm, init_, iter_, callback)
+
+    def __aiter__(self):
+        return self
+    
+    async def __anext__(self):
+        pkt = await self._ns.q.get()
+        self._ns.cv.acquire()
+        self._ns.cv.notify()
+        self._ns.cv.release()
+        return pkt
--- common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <system_error>
-
-static void check(errno_t err) {
-  if (err != A0_OK) {
-    throw std::system_error(err, std::generic_category(), "internal error");
-  }
-}
