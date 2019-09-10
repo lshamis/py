41b90fb7c50f24a832d52411c43f8763232eee0e
Leonid Shamis <leonid.shamis@gmail.com>
2019-09-10 14:02:13 -0700

    install aio_a0

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ module = Extension('a0',
                    sources = ['module.cc'],
                    extra_compile_args = ['-std=c++17', '-D_GLIBCXX_USE_CXX11_ABI=0'])
 
-setup (name = 'AlephZero',
-       version = '0.1',
-       description = 'TODO: description',
-       author = 'Leonid Shamis',
-       author_email = 'leonid.shamis@gmail.com',
-       url = 'https://github.com/alephzero/py',
-       long_description = '''TODO: long description''',
-       ext_modules = [module])
+setup(name = 'AlephZero',
+      version = '0.1',
+      description = 'TODO: description',
+      author = 'Leonid Shamis',
+      author_email = 'leonid.shamis@gmail.com',
+      url = 'https://github.com/alephzero/py',
+      long_description = '''TODO: long description''',
+      ext_modules = [module],
+      py_modules = ['aio_a0'])
