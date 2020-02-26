6809d9f40b58ad78b9324e78052382171ce8307b
Leonid Shamis <leonid.shamis@gmail.com>
2020-02-25 17:25:36 -0800

    Update for api changes

--- module.cc
+++ module.cc
@@ -28,65 +28,56 @@ PYBIND11_MODULE(alephzero_bindings, m) {
       .def_readwrite("size", &a0::Shm::Options::size);
 
   pyshmobj
-      .def(py::init<const std::string&>())
-      .def(py::init<const std::string&, const a0::Shm::Options&>())
+      .def(py::init<const std::string_view>())
+      .def(py::init<const std::string_view, const a0::Shm::Options&>())
       .def_property_readonly("path", &a0::Shm::path)
       .def_static("unlink", &a0::Shm::unlink);
 
   py::class_<a0::PacketView>(m, "PacketView")
-      .def_property_readonly("headers",
-                             [](a0::PacketView* pkt) {
-                               std::vector<std::pair<std::string_view, std::string_view>> hdrs;
-                               size_t num_hdrs = pkt->num_headers();
-                               for (size_t i = 0; i < num_hdrs; i++) {
-                                 hdrs.push_back(pkt->header(i));
-                               }
-                               return hdrs;
-                             })
-      .def_property_readonly("payload",
-                             [](a0::PacketView* self) {
-                               return py::bytes(std::string(self->payload()));
-                             })
-      .def_property_readonly("id", &a0::PacketView::id);
+      .def_property_readonly("id", &a0::PacketView::id)
+      .def_property_readonly("headers", &a0::PacketView::headers)
+      .def_property_readonly("payload", &a0::PacketView::payload);
 
   py::class_<a0::Packet>(m, "Packet")
       .def(py::init<>())
-      .def(py::init<a0::PacketView>())
-      .def(py::init<std::string_view>())
-      .def(py::init<const std::vector<std::pair<std::string_view, std::string_view>>&,
-                    std::string_view>())
-      .def_property_readonly("headers",
-                             [](a0::Packet* pkt) {
-                               std::vector<std::pair<std::string_view, std::string_view>> hdrs;
-                               size_t num_hdrs = pkt->num_headers();
-                               for (size_t i = 0; i < num_hdrs; i++) {
-                                 hdrs.push_back(pkt->header(i));
-                               }
-                               return hdrs;
-                             })
-      .def_property_readonly("payload",
-                             [](a0::Packet* self) {
-                               return py::bytes(std::string(self->payload()));
-                             })
-      .def_property_readonly("id", &a0::Packet::id);
+      .def(py::init<std::string>())
+      .def(py::init<std::vector<std::pair<std::string, std::string>>, std::string>())
+      .def(py::init<const a0::PacketView&>())
+      .def_property_readonly("id", &a0::Packet::id)
+      .def_property_readonly("headers", &a0::Packet::headers)
+      .def_property_readonly("payload", &a0::Packet::payload);
+
+  py::class_<a0::TopicAliasTarget>(m, "TopicAliasTarget")
+      .def_readwrite("container", &a0::TopicAliasTarget::container)
+      .def_readwrite("topic", &a0::TopicAliasTarget::topic);
 
   py::class_<a0::TopicManager>(m, "TopicManager")
-      .def(py::init<const std::string&>())
+      .def(py::init<>())
+      .def_readwrite("container", &a0::TopicManager::container)
+      .def_readwrite("subscriber_aliases", &a0::TopicManager::subscriber_aliases)
+      .def_readwrite("rpc_client_aliases", &a0::TopicManager::rpc_client_aliases)
+      .def_readwrite("prpc_client_aliases", &a0::TopicManager::prpc_client_aliases)
       .def("config_topic", &a0::TopicManager::config_topic)
+      .def("log_crit_topic", &a0::TopicManager::log_crit_topic)
+      .def("log_err_topic", &a0::TopicManager::log_err_topic)
+      .def("log_warn_topic", &a0::TopicManager::log_warn_topic)
+      .def("log_info_topic", &a0::TopicManager::log_info_topic)
+      .def("log_dbg_topic", &a0::TopicManager::log_dbg_topic)
       .def("publisher_topic", &a0::TopicManager::publisher_topic)
       .def("subscriber_topic", &a0::TopicManager::subscriber_topic)
       .def("rpc_server_topic", &a0::TopicManager::rpc_server_topic)
       .def("rpc_client_topic", &a0::TopicManager::rpc_client_topic);
 
   m.def("InitGlobalTopicManager", py::overload_cast<a0::TopicManager>(&a0::InitGlobalTopicManager));
-  m.def("InitGlobalTopicManager",
-        py::overload_cast<const std::string&>(&a0::InitGlobalTopicManager));
 
   py::class_<a0::Publisher>(m, "Publisher")
       .def(py::init<a0::Shm>())
-      .def(py::init<const std::string&>())
-      .def("pub", py::overload_cast<const a0::Packet&>(&a0::Publisher::pub))
-      .def("pub", py::overload_cast<std::string_view>(&a0::Publisher::pub));
+      .def(py::init<const std::string_view>())
+      .def("pub", py::overload_cast<const a0::PacketView&>(&a0::Publisher::pub))
+      .def("pub",
+           py::overload_cast<std::vector<std::pair<std::string, std::string>>,
+                             const std::string_view>(&a0::Publisher::pub))
+      .def("pub", py::overload_cast<const std::string_view>(&a0::Publisher::pub));
 
   py::enum_<a0_subscriber_init_t>(m, "SubscriberInit")
       .value("INIT_OLDEST", A0_INIT_OLDEST)
@@ -101,7 +92,7 @@ PYBIND11_MODULE(alephzero_bindings, m) {
 
   py::class_<a0::SubscriberSync>(m, "SubscriberSync")
       .def(py::init<a0::Shm, a0_subscriber_init_t, a0_subscriber_iter_t>())
-      .def(py::init<const std::string&, a0_subscriber_init_t, a0_subscriber_iter_t>())
+      .def(py::init<const std::string_view, a0_subscriber_init_t, a0_subscriber_iter_t>())
       .def("has_next", &a0::SubscriberSync::has_next)
       .def("next", &a0::SubscriberSync::next);
 
@@ -110,7 +101,7 @@ PYBIND11_MODULE(alephzero_bindings, m) {
                     a0_subscriber_init_t,
                     a0_subscriber_iter_t,
                     std::function<void(a0::PacketView)>>())
-      .def(py::init<const std::string&,
+      .def(py::init<const std::string_view,
                     a0_subscriber_init_t,
                     a0_subscriber_iter_t,
                     std::function<void(a0::PacketView)>>())
@@ -121,7 +112,7 @@ PYBIND11_MODULE(alephzero_bindings, m) {
                   py::arg("seek"),
                   py::arg("flags") = 0)
       .def_static("read_one",
-                  py::overload_cast<const std::string&, a0_subscriber_init_t, int>(
+                  py::overload_cast<const std::string_view, a0_subscriber_init_t, int>(
                       &a0::Subscriber::read_one),
                   py::arg("topic"),
                   py::arg("seek"),
@@ -131,53 +122,71 @@ PYBIND11_MODULE(alephzero_bindings, m) {
 
   py::class_<a0::RpcRequest>(m, "RpcRequest")
       .def_property_readonly("pkt", &a0::RpcRequest::pkt)
-      .def("reply", py::overload_cast<const a0::Packet&>(&a0::RpcRequest::reply))
-      .def("reply", py::overload_cast<std::string_view>(&a0::RpcRequest::reply));
+      .def("reply", py::overload_cast<const a0::PacketView&>(&a0::RpcRequest::reply))
+      .def("reply",
+           py::overload_cast<std::vector<std::pair<std::string, std::string>>,
+                             const std::string_view>(&a0::RpcRequest::reply))
+      .def("reply", py::overload_cast<const std::string_view>(&a0::RpcRequest::reply));
 
   py::class_<a0::RpcServer, nogil_holder<a0::RpcServer>>(m, "RpcServer")
       .def(py::init<a0::Shm,
                     std::function<void(a0::RpcRequest)>,
-                    std::function<void(std::string)>>())
-      .def(py::init<const std::string&,
+                    std::function<void(const std::string_view)>>())
+      .def(py::init<const std::string_view,
                     std::function<void(a0::RpcRequest)>,
-                    std::function<void(std::string)>>())
+                    std::function<void(const std::string_view)>>())
       .def("async_close", &a0::RpcServer::async_close);
 
   py::class_<a0::RpcClient, nogil_holder<a0::RpcClient>>(m, "RpcClient")
       .def(py::init<a0::Shm>())
-      .def(py::init<const std::string&>())
+      .def(py::init<const std::string_view>())
       .def("async_close", &a0::RpcClient::async_close)
       .def("send",
-           py::overload_cast<const a0::Packet&, std::function<void(a0::PacketView)>>(
+           py::overload_cast<const a0::PacketView&, std::function<void(const a0::PacketView&)>>(
                &a0::RpcClient::send))
       .def("send",
-           py::overload_cast<std::string_view, std::function<void(a0::PacketView)>>(
+           py::overload_cast<std::vector<std::pair<std::string, std::string>>,
+                             const std::string_view,
+                             std::function<void(const a0::PacketView&)>>(&a0::RpcClient::send))
+      .def("send",
+           py::overload_cast<const std::string_view, std::function<void(const a0::PacketView&)>>(
                &a0::RpcClient::send))
       .def("cancel", &a0::RpcClient::cancel);
 
   py::class_<a0::PrpcConnection>(m, "PrpcConnection")
       .def_property_readonly("pkt", &a0::PrpcConnection::pkt)
-      .def("send", py::overload_cast<const a0::Packet&, bool>(&a0::PrpcConnection::send))
-      .def("send", py::overload_cast<std::string_view, bool>(&a0::PrpcConnection::send));
+      .def("send", py::overload_cast<const a0::PacketView&, bool>(&a0::PrpcConnection::send))
+      .def("send",
+           py::overload_cast<std::vector<std::pair<std::string, std::string>>,
+                             const std::string_view,
+                             bool>(&a0::PrpcConnection::send))
+      .def("send", py::overload_cast<const std::string_view, bool>(&a0::PrpcConnection::send));
 
   py::class_<a0::PrpcServer, nogil_holder<a0::PrpcServer>>(m, "PrpcServer")
       .def(py::init<a0::Shm,
                     std::function<void(a0::PrpcConnection)>,
-                    std::function<void(std::string)>>())
-      .def(py::init<const std::string&,
+                    std::function<void(const std::string_view)>>())
+      .def(py::init<const std::string_view,
                     std::function<void(a0::PrpcConnection)>,
-                    std::function<void(std::string)>>())
+                    std::function<void(const std::string_view)>>())
       .def("async_close", &a0::PrpcServer::async_close);
 
   py::class_<a0::PrpcClient, nogil_holder<a0::PrpcClient>>(m, "PrpcClient")
       .def(py::init<a0::Shm>())
-      .def(py::init<const std::string&>())
+      .def(py::init<const std::string_view>())
       .def("async_close", &a0::PrpcClient::async_close)
       .def("connect",
-           py::overload_cast<const a0::Packet&, std::function<void(a0::PacketView, bool)>>(
+           py::overload_cast<const a0::PacketView&,
+                             std::function<void(const a0::PacketView&, bool)>>(
                &a0::PrpcClient::connect))
       .def("connect",
-           py::overload_cast<std::string_view, std::function<void(a0::PacketView, bool)>>(
+           py::overload_cast<std::vector<std::pair<std::string, std::string>>,
+                             const std::string_view,
+                             std::function<void(const a0::PacketView&, bool)>>(
+               &a0::PrpcClient::connect))
+      .def("connect",
+           py::overload_cast<const std::string_view,
+                             std::function<void(const a0::PacketView&, bool)>>(
                &a0::PrpcClient::connect))
       .def("cancel", &a0::PrpcClient::cancel);
 }
