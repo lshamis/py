377700454f678014e1734274b9d5727b45c40ac8
alephzero <48891271+alephzero@users.noreply.github.com>
2020-04-18 17:40:48 -0700

    Use pkg-config (#2)

--- setup.py
+++ setup.py
@@ -1,17 +1,48 @@
 from distutils.core import setup, Extension
 import pybind11
+import subprocess
+
+
+def pkgconfig(*packages, **kwargs):
+    config = kwargs.setdefault('config', {})
+    optional_args = kwargs.setdefault('optional', '')
+    
+    flag_map = {
+        'include_dirs': ['--cflags-only-I', 2],
+        'library_dirs': ['--libs-only-L', 2],
+        'libraries': ['--libs-only-l', 2],
+        'extra_compile_args': ['--cflags-only-other', 0],
+        'extra_link_args': ['--libs-only-other', 0],
+    }
+    for package in packages:
+        for distutils_key, (pkg_option, n) in flag_map.items():
+            items = subprocess.check_output(['pkg-config', optional_args, pkg_option, package]).decode('utf8').split()
+            config.setdefault(distutils_key, []).extend([i[n:] for i in items])
+    return config
+
+
+def merge_flags(*flags_groups):
+    all_flags = {}
+    for flags_group in flags_groups:
+        for key, vals in flags_group.items():
+            all_flags.setdefault(key, []).extend(vals)
+    return all_flags
+
 
 module = Extension('alephzero_bindings',
-                   include_dirs = [
-                       '/usr/include',
-                        pybind11.get_include(True),
-                        pybind11.get_include(False),
-                   ],
-                   library_dirs = ['/usr/lib'],
-                   libraries = ['alephzero'],
                    sources = ['module.cc'],
-                   extra_compile_args = ['-std=c++17'],
-                   extra_link_args=['-lrt'])
+                   **merge_flags(
+                       pkgconfig('alephzero'),
+                       {
+                           'include_dirs': [
+                               pybind11.get_include(True),
+                               pybind11.get_include(False),
+                           ],
+                           'extra_compile_args': [
+                               '-std=c++17',
+                           ],
+                       }
+                   ))
 
 setup(name = 'alephzero',
       version = '0.1',
