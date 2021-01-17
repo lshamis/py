665aaf7ebfd54a53b0a9aecc9efef793faf613bf
alephzero <48891271+alephzero@users.noreply.github.com>
2021-01-17 12:10:15 -0800

    Version bump (#22)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ module = Pybind11Extension('alephzero_bindings',
                            extra_compile_args=['-std=c++17', '-O3'])
 
 setup(name='alephzero',
-      version='0.2.4',
+      version='0.2.5',
       description='TODO: description',
       author='Leonid Shamis',
       author_email='leonid.shamis@gmail.com',
